6e2b6d5b36f895c1d4d97efe50c08f271d752d67
specowos <77427029+specowos@users.noreply.github.com>
2021-01-13 22:49:34 -0600

    Add files via upload

---
+++ SpecR.lua
@@ -0,0 +1,11 @@
+--[[                                        
+  ___ _ __   ___  ___ 
+/ __|  _ \ / _ \/ __|
+\__ \ |_) |  __/ (__ 
+|___/  __/ \___|\___|
+     | |              
+     |_|              
+]]
+--f pastebin
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/Spec-R/main/SpecR.lua"))()
